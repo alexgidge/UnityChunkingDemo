d8fe06934e156cf1aa4ed6f9d8b2e421c49a47b0
Alex <development@alexgidge.com>
2021-08-31 20:46:06 +0100

    Finished prefab level generation
    Small bugs still to patch (exception thrown on returning to same coordinate)

--- Assets/Scripts/LevelGenerationController.cs
+++ Assets/Scripts/LevelGenerationController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -15,48 +17,119 @@ public class LevelGenerationController : MonoBehaviour
     public List<GameObject> SectionPrefabs;
     
     [SerializeField]
-    private List<GameObject> SectionsInPlay;
+    private Dictionary<Vector2, GameObject> ChunksInPlay;
+
+    private const string WEST_COLLIDER_TAG = "WestCollider";
+    private const string EAST_COLLIDER_TAG = "EastCollider";
+    private const string NORTH_COLLIDER_TAG = "NorthCollider";
+    private const string SOUTH_COLLIDER_TAG = "SouthCollider";
 
     private void Start()
     {
         Current = this;
+        ChunksInPlay = new Dictionary<Vector2, GameObject>();
+        //TODO: Load first block 
     }
 
-    private void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (MovementController.Current.transform.position.x > 10 && SectionsInPlay.Count < 3)
+        GameObject parent = other.transform.parent.gameObject;
+        //TODO: Refactor
+        switch (other.tag)
         {
-            LoadNextSection();
+            case NORTH_COLLIDER_TAG:
+                LoadNextChunk(parent, DirectionType.North);
+                break;
+            case EAST_COLLIDER_TAG:
+                LoadNextChunk(parent, DirectionType.East);
+                break;
+            case SOUTH_COLLIDER_TAG:
+                LoadNextChunk(parent, DirectionType.South);
+                break;
+            case WEST_COLLIDER_TAG:
+                LoadNextChunk(parent, DirectionType.West);
+                break;
         }
     }
 
-    void LoadNextSection()
+    void LoadNextChunk(GameObject colliderParent, DirectionType direction)
     {
-        GameObject newSection = Instantiate(GetRandomSection(), LevelGrid.transform);
-        SectionsInPlay.Add(newSection); 
-        SectionCleanup();
+        GameObject newChunk = Instantiate(GetRandomChunkPrefab(), LevelGrid.transform);
+
+        Vector2 chunkLocation = GetNextChunkLocation(colliderParent, direction);
+        CacheChunk(chunkLocation, newChunk);
+        SetChunkPosition(chunkLocation, newChunk);
+        ChunkCleanup();
     }
 
-    void SectionCleanup()
+    private Vector2 GetNextChunkLocation(GameObject colliderParent, DirectionType direction)
     {
-        foreach (GameObject section in SectionsInPlay)
+        KeyValuePair<Vector2, GameObject> currentChunkLocation = ChunksInPlay.Where(x => x.Value == colliderParent).FirstOrDefault();
+
+        Vector2 chunkLocation = new Vector2();
+        
+        //TODO: Refactor
+        switch (direction)
         {
-            if (section != null)
+            case DirectionType.North:
+                chunkLocation = currentChunkLocation.Key + new Vector2(0, 1);
+                break;
+            case DirectionType.East:
+                chunkLocation = currentChunkLocation.Key + new Vector2(1,0);
+                break;
+            case DirectionType.South:
+                chunkLocation = currentChunkLocation.Key + new Vector2(0, -1);
+                break;
+            case DirectionType.West:
+                chunkLocation = currentChunkLocation.Key + new Vector2(-1,0);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+        
+        return chunkLocation;
+    }
+
+    private void SetChunkPosition(Vector2 gridLocation, GameObject newSection)
+    {
+        newSection.transform.position = new Vector3(gridLocation.x * SectionSize.x, gridLocation.y * SectionSize.y);
+    }
+
+    private void CacheChunk(Vector2 gridLocation, GameObject newSection)
+    {
+        if (!ChunksInPlay.ContainsValue(newSection))
+        {
+            ChunksInPlay.Add(gridLocation, newSection);
+        }
+    }
+
+    void ChunkCleanup()
+    {
+        foreach (KeyValuePair<Vector2, GameObject> section in ChunksInPlay)
+        {
+            if (section.Value != null)
             {
-                if (Vector3.Distance(section.transform.position, MovementController.Current.transform.position) >
+                if (Vector3.Distance(section.Value.transform.position, MovementController.Current.transform.position) >
                     DestroyDistance)
                 {
-                    SectionsInPlay.Remove(section);
-                    Destroy(section);
+                    Destroy(section.Value);
                 }
             }
         }
     }
 
-    GameObject GetRandomSection()
+    GameObject GetRandomChunkPrefab()
     {
         int x = SectionPrefabs.Count -1;
         int rand = Random.Range(0, x);
         return SectionPrefabs[rand];
     }
+}
+
+enum DirectionType
+{
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
 }
