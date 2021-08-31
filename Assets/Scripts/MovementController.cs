fa0dbd168e711f14642333b379dfd615f30c3a99
Alex <development@alexgidge.com>
2021-08-31 16:32:06 +0100

    Base scene created with player movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovementController : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
     public int MoveSpeed; //10
     
