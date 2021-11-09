daacf6fd384f1c581d680836c5e0d779524eddea
Alex <development@alexgidge.com>
2021-11-09 13:00:10 +0000

    v1.1

--- Assets/Scripts/LevelGenerationController.cs
+++ Assets/Scripts/LevelGenerationController.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class LevelGenerationController : MonoBehaviour
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class MovementController : MonoBehaviour
 {
